e0371a921b202fcf908f562fd77eaeaddee59c41
vijayaraj-at-207315043547 <vijayaraj@claritaz.com>
2023-02-05 17:06:12 +0530

    changed private routes

--- routes/privateRoutes/admin.tsx
+++ routes/privateRoutes/admin.tsx
@@ -1,9 +1,22 @@
+import { useRouter } from "next/router";
+import { useContext } from "react";
 import Footer from "../../container/footer";
 import Header from "../../container/header";
+import { DataContext } from "../../context/user";
 
 const AdminAuth = (WrappedComponent: any) => {
     return (props: any) => {
+        const {state}=useContext(DataContext)
+        const router = useRouter();
+        if(typeof window !=='undefined'){
+            if (!(state?.auth?.user?.role.includes('ADMIN'))) {
+                router.replace("/");
+                return null; 
+              }
+        
         return <><Header /><WrappedComponent {...props} /><Footer /></>
+    }
+        return null
     }
 }
 export default AdminAuth
--- routes/privateRoutes/seller.tsx
+++ routes/privateRoutes/seller.tsx
@@ -1,13 +1,22 @@
+import { useRouter } from "next/router";
 import { useContext } from "react";
 import Footer from "../../container/footer";
 import Header from "../../container/header";
 import { DataContext } from "../../context/user";
-import { useRouter } from "next/router";
 
-const SellerAuth = (WrappedComponent: any) => {
+const AdminAuth = (WrappedComponent: any) => {
     return (props: any) => {
-        const { state } = useContext(DataContext)
-            return <><Header /><WrappedComponent {...props} /><Footer /></>
+        const {state}=useContext(DataContext)
+        const router = useRouter();
+        if(typeof window !=='undefined'){
+            if (!(state?.auth?.user?.role.includes('SELLER'))) {
+                router.replace("/");
+                return null; 
+              }
+        
+        return <><Header /><WrappedComponent {...props} /><Footer /></>
+    }
+        return null
     }
 }
-export default SellerAuth
+export default AdminAuth
