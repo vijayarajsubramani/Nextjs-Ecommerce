56fd15c61264b1f93a80a3e64781ad552fc45456
vijayarajsubramani <vijayarajsubramani@gmail.com>
2023-02-08 17:48:58 +0530

    bulkupload admin

--- component/ProductFilter/index.tsx
+++ component/ProductFilter/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react"
+import request from "../../service/base.service"
+
 interface Tprops {
     setsearchvalue?: (e: string) => void,
     setSortObj?: (e: any) => void
-    setFilterObj?:any
+    setFilterObj?: any
 }
-const ProductFilter: React.FC<Tprops> = ({ setsearchvalue, setSortObj ,setFilterObj}) => {
+const ProductFilter: React.FC<Tprops> = ({ setsearchvalue, setSortObj, setFilterObj }) => {
+    const [categoryList, setCategoryList] = useState([])
+
+    const categoryNames = async () => {
+        await request({ url: '/api/category/category', method: 'get' }).then((res) => {
+            if (res.status === 'success') {
+                setCategoryList(res?.data)
+            }
+        })
+    }
     const searchProduct = (value: any) => {
         if (setsearchvalue) {
             setsearchvalue(value)
@@ -16,24 +28,28 @@ const ProductFilter: React.FC<Tprops> = ({ setsearchvalue, setSortObj ,setFilter
     }
     const categoryFilter = (value: any) => {
         if (setFilterObj) {
-            setFilterObj({categoryName:value})
+            setFilterObj({ categoryName: value })
         }
     }
+    useEffect(() => {
+        categoryNames()
+    }, [])
     return (
         <>
-            <div style={{ width: '99%' ,cursor:'pointer'}} className='d-flex m-2 row border shadow p-2 rounded'>
-                <div className='col-7 d-flex justify-content-end'>
-                    <select className="w-25 mx-1" onChange={(e:any)=>categoryFilter(e.target.value)}>
-                        <option value=''>Category</option>
-                        <option value='mobile'>Mobile</option>
-                        <option value='shirts'>Shirts</option>
-                        <option value='laptop'>Laptop</option>
-                        <option value='television'>Television</option>
+            <div style={{ width: '99%', cursor: 'pointer' }} className='d-flex m-2 row border shadow p-2 rounded'>
+                <div className='col d-flex justify-content-end'>
+                    <select className="w-25 mx-1" onChange={(e: any) => categoryFilter(e.target.value)}>
+                        <option value=''>Choose Category</option>
+                        {categoryList?.length ? categoryList?.map((item: any, index) => {
+                            return (
+                                <option key={index} value={item._id}>{item.name}</option>
+                            )
+                        }) : <option></option>}
                     </select>
-                    <input className="border-radius-2 w-50 " placeholder="Search Product" type='text' onChange={(e: any) => { searchProduct(e.target.value) }} />
+                        <input className="border-radius-2 w-50 " placeholder="Search Product" type='text' onChange={(e: any) => { searchProduct(e.target.value) }} />
                 </div>
                 <div className='col-5 d-flex justify-content-end' >
-                    <select className="rounded-pill w-25" onChange={(e:any)=>sortProduct(e.target.value)}>
+                    <select className="rounded-pill w-25" onChange={(e: any) => sortProduct(e.target.value)}>
                         <option value='price-ASC'>Sort</option>
                         <option value='createdAt-DESC'>ASC</option>
                         <option value='createdAt-DESC'>DESC</option>
